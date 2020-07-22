183264f6e4b7372829eb4564dcfa8bcf02e0d9b9
slreidinger <slreidinger@gmail.com>
2020-07-21 20:35:24 -0400

    Removed example code, setup main admin login page, added some options to the nav menu

---
+++ Personal Website/Classes/Helpers/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Personal_Website.Classes.Helpers {
+    public static class Helper {
+
+        public static string DisplayNameFor(Type modelType, string propertyName)
+        {
+            var attribute = modelType.GetProperty(propertyName).GetCustomAttribute(typeof(DisplayAttribute)) as DisplayAttribute;
+
+            if (attribute == null) return string.Empty;
+
+            return attribute.Name;
+        }
+
+    }
+}
---
+++ Personal Website/Data/Models/PasswordField.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Personal_Website.Data.Models {
+    public class PasswordField {
+
+        [Required]
+        [Description("Enter Admin Password:")]
+        [Display(Name = "Enter Admin Password:")]
+        [PasswordPropertyText]
+        public string Password { get; set; }
+
+    }
+}
