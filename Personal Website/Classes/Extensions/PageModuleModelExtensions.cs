4d60956d1c9870a2da896769a8da3c5bfea33ae7
slreidinger <slreidinger@gmail.com>
2020-07-26 19:31:51 -0400

    added controls for adding new modules

---
+++ Personal Website/Classes/Extensions/PageModuleModelExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Personal_Website.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Personal_Website.Classes.Extensions {
+    public static class PageModuleModelExtensions {
+
+        public static bool IsEmpty(this PageModuleModel model)
+        {
+            bool isEmpty = true;
+
+            if (string.IsNullOrWhiteSpace(model.Title) == false) isEmpty = false; 
+            if (string.IsNullOrWhiteSpace(model.Summary) == false) isEmpty = false; 
+
+            return isEmpty;
+        }
+
+    }
+}
