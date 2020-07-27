f559ee27c7f848190ee9b10b0f566033f629892b
slreidinger <slreidinger@gmail.com>
2020-07-27 15:49:42 -0400

    Made a little progress on resizing summary textarea, changed some parent div classes (formatting)

--- Personal Website/Data/Models/PageModuleModel.cs
+++ Personal Website/Data/Models/PageModuleModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Personal_Website.Data.Models {
@@ -10,7 +11,26 @@ namespace Personal_Website.Data.Models {
         public int Id { get; set; }
         public int Type { get; set; }
         public string Title { get; set; }
-        public string Summary { get; set; }
+        public string Summary
+        {
+            get => _summary;
 
+            set
+            {
+                _summary = value;
+                ResizeSummaryRows(value);
+            }
+        }
+
+        [NotMapped]
+        private string _summary { get; set; }
+
+        [NotMapped]
+        public int SummaryRows { get; set; }
+
+        private void ResizeSummaryRows(string value)
+        {
+            SummaryRows = value.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None).Length;
+        }
     }
 }
