9c22420b385b106c3122ee6a1862ac4a5341e06d
slreidinger <slreidinger@gmail.com>
2020-07-29 13:28:56 -0400

    Added email contact component, no function

---
+++ Personal Website/Data/Models/ContactEmailModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Personal_Website.Data.Models {
+    public class ContactEmailModel {
+
+        [Required]
+        [Display(Name = " Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = " Email")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [Display(Name = " Message")]
+        public string Message {
+            get => _message;
+
+            set {
+                _message = value;
+                ResizeMessageRows(value);
+            }
+        }
+
+        private string _message { get; set; }
+
+        public int MessageRows { get; set; }
+
+        private void ResizeMessageRows(string value)
+        {
+            MessageRows = value.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None).Length;
+        }
+
+        public string RemoteIPAddress { get; set; }
+    }
+}
