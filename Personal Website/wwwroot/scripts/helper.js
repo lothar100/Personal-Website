1da927a27607a978c9ffaffb9e3fad9c057b6e2e
slreidinger <slreidinger@gmail.com>
2020-08-22 16:23:16 -0400

    Almost finished show more button animation. Need to fix bug with initial heights not working if user resizes screen.

--- Personal Website/wwwroot/scripts/helper.js
+++ Personal Website/wwwroot/scripts/helper.js
@@ -1,6 +1,12 @@
 ﻿
 window.helper = {
+
     getHeight: function (id) {
         return document.getElementById(id).offsetHeight;
+    },
+
+    getWidth: function (id) {
+        return document.getElementById(id).offsetWidth;
     }
+
 }
